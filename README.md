75367783308b6303480b9c22bbbec6dfde2d3c7c
Lev Kadatskyi <levkadatscky@gmail.com>
2024-10-27 14:15:26 +0200

    added requirements.txt, formatted graph.py, added README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# Graphmap-algorithms
+
+# Technology Stack
+
+[![My Skills](https://skillicons.dev/icons?i=py)](https://skillicons.dev)
+
+To run: 
+- create virtual environment
+
+```
+python -m venv venv
+```
+- activate it
+```
+venv/Scripts/activate
+```
+- install all dependencies
+```
+pip install -r requirements.txt
+```
+- type ``python graph.py`` to execute file
--- graph.py
+++ graph.py
@@ -1,8 +1,9 @@
-import osmnx as ox
-import networkx as nx
-import matplotlib.pyplot as plt
-import os
 import json
+import os
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import osmnx as ox
 
 # Указываем начальные и конечные координаты (широта, долгота)
 start_point = (50.02418570642534, 36.18335487604594)
@@ -18,13 +19,14 @@ custom_filter = (
 )
 
 # Указываем путь к файлу GraphML
-graphml_file = 'ukraine_graph.graphml'
+graphml_file = "ukraine_graph.graphml"
 
 if os.path.exists(graphml_file):
     G = ox.load_graphml(graphml_file)
 else:
-    G = ox.graph_from_place("Ukraine", network_type="drive",
-                            simplify=True, custom_filter=custom_filter)
+    G = ox.graph_from_place(
+        "Ukraine", network_type="drive", simplify=True, custom_filter=custom_filter
+    )
 
     ox.save_graphml(G, filepath=graphml_file)
 
@@ -37,8 +39,7 @@ if nx.has_path(G, start_node, end_node):
     # Находим кратчайший путь между двумя узлами с использованием алгоритма Дейкстры
     shortest_path = nx.shortest_path(G, start_node, end_node, weight="length")
 
-    route_coords = [(G.nodes[node]['y'], G.nodes[node]['x'])
-                    for node in shortest_path]
+    route_coords = [(G.nodes[node]["y"], G.nodes[node]["x"]) for node in shortest_path]
 
     route_data = {"route": route_coords}
 
