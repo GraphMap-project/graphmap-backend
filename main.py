8b7b112f85b5cd32e9e01519c7c6b4b55f4749db
Lev Kadatskyi <levkadatscky@gmail.com>
2024-11-01 14:20:24 +0200

    added function for getting distance matrix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import numpy as np
 import osmnx as ox
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
@@ -56,5 +57,42 @@ def get_shortest_path(request: RouteRequest):
                 status_code=404,
                 detail="Невозможно проложить маршрут между двумя точками.",
             )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/distance_matrix")
+def get_distance_matrix(request: RouteRequest):
+    G = load_graph(graphml_file, custom_filter)
+
+    start_point = request.start_point
+    end_point = request.end_point
+
+    try:
+        # Находим ближайшие узлы графа к этим точкам
+        start_node = ox.nearest_nodes(G, start_point[1], start_point[0])
+        end_node = ox.nearest_nodes(G, end_point[1], end_point[0])
+
+        print(start_node, end_node)
+
+        distance_matrix = np.zeros((len(request), len(request)))
+
+        for i in range(len(request)):
+            for j in range(len(request)):
+                if i == j:
+                    distance_matrix[i, j] = 0
+                else:
+                    # TODO: Ask about distance between nodes
+
+                    distance_matrix[i, j] = nx.shortest_path_length(
+                        G,
+                        source=start_node,
+                        target=end_node,
+                        weight="length",
+                    )
+
+        return {"distance_matrix": distance_matrix.tolist()}
+    except nx.NodeNotFound:
+        raise HTTPException(status_code=404, detail="Одна из точек не найдена в графе.")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- schemas/route_request.py
+++ schemas/route_request.py
@@ -6,3 +6,13 @@ from pydantic import BaseModel, conlist
 class RouteRequest(BaseModel):
     start_point: conlist(float, min_length=2, max_length=2)
     end_point: conlist(float, min_length=2, max_length=2)
+
+    # TODO: add intermediate points
+    """
+    intermediate_points: List[conlist(float, min_length=2, max_length=2)]
+    point_lst = [self.start_point, self.end_point, self.intermediate_points]
+    """
+
+    def __len__(self):
+        point_lst = [self.start_point, self.end_point]
+        return len(point_lst)
