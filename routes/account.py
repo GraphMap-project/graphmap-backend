73972aa9d34e45466e39bfa17cac66763079af35
Lev Kadatskyi <levkadatscky@gmail.com>
2025-01-29 13:52:24 +0200

    updated user registration

--- routes/account.py
+++ routes/account.py
@@ -1,18 +1,46 @@
+from datetime import datetime, timedelta
+
 from fastapi import APIRouter, HTTPException
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from sqlmodel import select
 
 from config.database import SessionDep
+from config.jwt_config import *
 from models.user import User
 from schemas.userCreate import UserCreate
 
 account = APIRouter()
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
 
 @account.post("/register")
 def register(user: UserCreate, session: SessionDep):
-    existing_user = session.query(User).filter(User.email == user.email).first()
+    statement = select(User).where(User.email == user.email)
+    existing_user = session.exec(statement).first()
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already exists")
-    new_user = User(email=user.email, password=user.password)
+
+    hashed_password = hash_password(user.password)
+
+    new_user = User(email=user.email, password=hashed_password)
 
     session.add(new_user)
     session.commit()
--- schemas/userCreate.py
+++ schemas/userCreate.py
@@ -1,6 +1,39 @@
-from pydantic import BaseModel, EmailStr
+import re
+
+from pydantic import BaseModel, EmailStr, ValidationError, field_validator
 
 
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, password: str):
+        errors = {}
+
+        if not (8 <= len(password) <= 50):
+            errors["length"] = "Password must be between 8 and 50 characters long"
+
+        if not re.search(r"[a-z]", password):
+            errors["lowercase"] = "Password must contain at least one lowercase letter"
+
+        if not re.search(r"[A-Z]", password):
+            errors["uppercase"] = "Password must contain at least one uppercase letter"
+
+        if not re.search(r"\d", password):
+            errors["digit"] = "Password must contain at least one digit"
+
+        if not re.search(r"[@$!%*?&]", password):
+            errors["special"] = (
+                "Password must contain at least one special character (@$!%*?&)"
+            )
+
+        if errors:
+            raise ValueError("Password validation failed", errors)
+
+        return password
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate_password
